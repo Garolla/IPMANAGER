e21fb406975cfb3d6c3992b2fd1b776915412df7
BellinoSalvatore <bellinosalvatore1@gmail.com>
2017-05-24 12:24:31 +0200

    CONSTANTS

---
+++ CONSTANTS.vhd
@@ -0,0 +1,15 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package CONSTANTS is
+
+constant DATA_WIDTH : integer := 16;
+constant ADD_WIDTH  : integer := 6;
+end CONSTANTS;
+
+package body CONSTANTS is
+
+ 
+end CONSTANTS;
